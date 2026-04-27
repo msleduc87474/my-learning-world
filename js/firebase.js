3f657ee2d0fd982687334ace2d9f71fcfab8bf93
msleduc87474 <msleduc84@gmail.com>
2026-04-26 21:22:56 -0400

    Refactor Firebase initialization code
    Removed script tags and initialized Firebase configuration.

--- js/firebase.js
+++ js/firebase.js
@@ -1,19 +1,8 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyDvdTuP3p7U5AE432e3N0dyt4Q_4e2ZFIQ",
-    authDomain: "my-learning-world-1fb9a.firebaseapp.com",
-    projectId: "my-learning-world-1fb9a",
-    storageBucket: "my-learning-world-1fb9a.firebasestorage.app",
-    messagingSenderId: "292318862711",
-    appId: "1:292318862711:web:a2ce6c343f45a4838c5572"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+const firebaseConfig = {
+  apiKey: "AIzaSyDvdTuP3p7U5AE432e3N0dyt4Q_4e2ZFIQ",
+  authDomain: "my-learning-world-1fb9a.firebaseapp.com",
+  projectId: "my-learning-world-1fb9a",
+  storageBucket: "my-learning-world-1fb9a.firebasestorage.app",
+  messagingSenderId: "292318862711",
+  appId: "1:292318862711:web:f8c41f1cd9480daf8c5572"
+};
