ddc716ffc87dbeedfd120311f928c24dda68b4a4
msleduc87474 <msleduc84@gmail.com>
2026-04-30 21:32:01 -0400

    Delete js directory

--- js/engine.js
+++
@@ -1,8 +0,0 @@
-function saveProgress(){
-  const user = auth.currentUser;
-  if(!user) return;
-
-  db.collection("students").doc(user.uid).set({
-    progress: "updated"
-  }, { merge:true });
-}
--- js/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDvdTuP3p7U5AE432e3N0dyt4Q_4e2ZFIQ",
-  authDomain: "my-learning-world-1fb9a.firebaseapp.com",
-  projectId: "my-learning-world-1fb9a",
-  storageBucket: "my-learning-world-1fb9a.firebasestorage.app",
-  messagingSenderId: "292318862711",
-  appId: "1:292318862711:web:f8c41f1cd9480daf8c5572"
-};
